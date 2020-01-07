3c65026bb72e2ae753123f3f4843cf8f9f718ceb
Mathieu Lapointe <mathieu.lapointe3@mail.mcgill.ca>
2020-01-07 15:32:46 -0500

    remove bash_profile since I use fish as login shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,18 +5,14 @@ alias c="cd"
 alias ..=".."
 alias v="nvim"
 alias c="cargo"
+alias g="git"
 
-# git
-alias gs="git status"
-alias ga="git add"
-alias gc="git commit"
-alias gp="git push"
-
+# Set nvim as default editor
 set -Ux EDITOR nvim
 
 # autojump
-[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+# [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
 
 # Start fish in tmux session
-test $TERM != "screen"; and exec tmux
-set -g fish_user_paths "/usr/local/opt/qt/bin" $fish_user_paths
+# test $TERM != "screen"; and exec tmux
+# set -g fish_user_paths "/usr/local/opt/qt/bin" $fish_user_paths
