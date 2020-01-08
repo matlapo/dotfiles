ee41a0952e4868942bab2bdb657b921444a5b643
Mathieu Lapointe <mathieu.lapointe3@mail.mcgill.ca>
2020-01-08 16:49:36 -0500

    adapt bootstrap.sh to zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,10 +17,10 @@ function doIt() {
 	# reset env option too
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [ "$1" = "--force" ] || [ "$1" = "-f" ]; then
 	doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	read -q '?This may overwrite existing files in your home directory. Are you sure? (y/n)? ';
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt;
