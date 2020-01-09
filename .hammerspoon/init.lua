5137de4a4956e6374bdc901cff658de1a6c40bd3
Mathieu Lapointe <mathieu.lapointe3@mail.mcgill.ca>
2020-01-09 15:50:09 -0500

    init.lua

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,8 @@
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
+  hs.reload()
+end)
+hs.alert.show("Config loaded")
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+  hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
+end)
