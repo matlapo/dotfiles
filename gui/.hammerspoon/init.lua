5c938914c5c771cd45e23f265956f563f7a9456e
Mathieu Lapointe <matlapo.dev@gmail.com>
2020-06-02 21:07:53 -0400

    wip

--- gui/.hammerspoon/init.lua
+++
@@ -1,8 +0,0 @@
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
-  hs.reload()
-end)
-hs.alert.show("Config loaded")
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-  hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
-end)
